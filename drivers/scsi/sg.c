64d1adef46767cbd29bb1f7f7d769b5d9c664b69
Alexander Potapenko <glider@google.com>
2018-05-18 16:23:18 +0200

    scsi: sg: allocate with __GFP_ZERO in sg_build_indirect()
    applied by CVE kernel patcher:
    https://codeberg.org/AXP-OS/kernel_patches/src/branch/main/CVE-2018-1000204/3.18/0004.patch

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -1837,7 +1837,7 @@ retry:
 		num = (rem_sz > scatter_elem_sz_prev) ?
 			scatter_elem_sz_prev : rem_sz;
 
-		schp->pages[k] = alloc_pages(gfp_mask, order);
+		schp->pages[k] = alloc_pages(gfp_mask | __GFP_ZERO, order);
 		if (!schp->pages[k])
 			goto out;
 
@@ -1998,11 +1998,12 @@ sg_get_rq_mark(Sg_fd * sfp, int pack_id)
 		if ((1 == resp->done) && (!resp->sg_io_owned) &&
 		    ((-1 == pack_id) || (resp->header.pack_id == pack_id))) {
 			resp->done = 2;	/* guard against other readers */
-			break;
+			write_unlock_irqrestore(&sfp->rq_list_lock, iflags);
+			return resp;
 		}
 	}
 	write_unlock_irqrestore(&sfp->rq_list_lock, iflags);
-	return resp;
+	return NULL;
 }
 
 /* always adds to end of list */
